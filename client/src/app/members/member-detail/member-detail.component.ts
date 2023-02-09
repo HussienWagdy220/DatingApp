ad2d41a7718116075d3081e0217071f392c9b06f
Hussien Wagdy <hussienwagdy220@gmail.com>
2023-02-10 01:10:10 +0200

    End of section 9

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery/public-api';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  member: Member | undefined;
 
-  constructor() { }
+
+  constructor(private memberService: MembersService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadmember();
+
+
+  }
+
+  getImages(){
+    if(!this.member) return [];
+    const imageUrls = [];
+    for(const photo of this.member.photos) {
+      imageUrls.push({
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url
+      })
+    }
+    return imageUrls;
+  }
+
+  loadmember(){
+    const username = this.route.snapshot.paramMap.get('username');
+    if(!username) return;
+    this.memberService.getMember(username).subscribe({
+      next: member => this.member = member
+    })
   }
 
 }
