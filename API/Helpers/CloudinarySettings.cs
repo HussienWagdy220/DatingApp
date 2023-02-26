913fd1a97cb5e8850ae4187a44377e1eaad5d630
Hussien Wagdy <hussienwagdy220@gmail.com>
2023-02-26 03:10:07 +0200

    End of section 11

---
+++ API/Helpers/CloudinarySettings.cs
@@ -0,0 +1,11 @@
+using System.Reflection.Metadata.Ecma335;
+
+namespace API.Helpers
+{
+    public class CloudinarySettings
+    {
+        public string CloudName { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+}
